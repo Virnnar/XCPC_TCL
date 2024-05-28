4cbea428eae74acf5259989881b1671f3cf1b4c4
cy <12546716@qq.com>
2024-05-28 18:52:25 +0800

    5.30

--- cccycym/1794C.cpp
+++ cccycym/1794C.cpp
@@ -7,8 +7,31 @@
 using namespace std;
 const int N = 1E5 + 10, mod = 1E9 + 7;
 
+void run()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    for(int i = 1; i <= n; i ++ ) cin >> a[i];
+
+    for(int i = 1; i <= n; i ++ )
+    {
+        int l = 1, r = i;
+        while(l < r)
+        {
+            int mid = l + r + 1 >> 1;
+            if(mid <= a[i - (mid - 1)]) l = mid;
+            else r = mid - 1;
+        }
+        cout << l << ' ';
+    }
+    cout << endl;
+}
+
 int main()
 {
-
+    int T;
+    cin >> T;
+    while(T -- ) run();
     return 0;
 }
