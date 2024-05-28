7218edc2a7b47873f9fd3ce4fd4e4312b90b348f
cy <12546716@qq.com>
2024-05-28 20:55:05 +0800

    ...

--- cccycym/1864C.cpp
+++ cccycym/1864C.cpp
@@ -7,8 +7,41 @@
 using namespace std;
 const int N = 1E5 + 10, mod = 1E9 + 7;
 
+void run()
+{
+    int x, p;
+    cin >> x;
+    vector<int> op(1, x);
+
+    for(int i = 0; ; i ++ )
+        if(x >> i & 1)
+        {
+            if(x == 1 << i)
+            {
+                p = i;
+                break;
+            }
+            x -= 1 << i;
+            op.push_back(x);
+        }
+
+    while(p)
+    {
+        x -= 1 << p - 1;
+        op.push_back(x);
+        p --;
+    }
+
+    cout << op.size() << endl;
+    for(auto i : op) cout << i << ' ';
+    cout << endl;
+}
+
+
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while(T -- ) run();
     return 0;
 }
