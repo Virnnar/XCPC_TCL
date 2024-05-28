66a4aab2a106ebc5cbf15b699596d085b98c5ab6
cy <12546716@qq.com>
2024-05-28 20:27:20 +0800

    ...

--- cccycym/1857E.cpp
+++ cccycym/1857E.cpp
@@ -7,8 +7,42 @@
 using namespace std;
 const int N = 1E5 + 10, mod = 1E9 + 7;
 
+void run()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    vector<LL>  ans(n);
+
+    for(int i = 0; i < n; i ++ )
+    {
+        int x;
+        cin >> x;
+        a[i] = {x, i};
+    }
+
+    sort(a.begin(), a.end());
+
+    LL dis = 0;
+    for(int i = 0; i < n; i ++ ) dis += a[i].first - a[0].first;
+    ans[a[0].second] = dis;
+
+    for(int i = 1; i < n; i ++ ) 
+    {
+        LL dx = a[i].first - a[i - 1].first;
+        dis += max(0, i - 1) * dx;
+        dis -= max(0, n - (i + 1)) * dx;
+        ans[a[i].second] = dis;
+    }
+
+    for(int i = 0; i < n; i ++ ) cout << ans[i] + n << ' ';
+    cout << endl;
+}
+
 int main()
 {
-
+    int T;
+    cin >> T;
+    while(T -- ) run();
     return 0;
 }
