50d9518a42d2620041576e80e258a14405326a2a
Virmar <virmarqy@gmail.com>
2024-05-28 15:56:04 +0800

    5.28

--- Lin/title/P_1001_A_B_Problem.cpp
+++ Lin/title/P_1001_A_B_Problem.cpp
@@ -5,7 +5,6 @@
 #include<queue>
 #include<map>
 #include<unordered_map>
-#include<algorithm>
 
 #define mset(a, t) memset(a, t, sizeof a)
 
@@ -14,8 +13,7 @@ namespace virmar {
     using ll = long long;
 
     const int N_ = 100000;
-
-    bool READ_ARC = false;
+    bool READ_ARG = false;
 
     void optimize() {
         ios::sync_with_stdio(false);
@@ -23,13 +21,13 @@ namespace virmar {
     }
 
     char buf[1<<21],*p1=buf,*p2=buf;
-    inline int getc(int arg = READ_ARC){
+    inline int getc(bool arg = 0){
         if (arg == false) return getchar();
         return p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++;
     }
 
     template<typename T>
-    inline T read(int arg = 0) {
+    inline T read(int arg = READ_ARG) {
         T x = 0, f = 1;
         char c = getc(arg);
         while (!isdigit(c)) {if (c == '-') f = -1; c = getc(arg);}
@@ -67,13 +65,14 @@ namespace virmar {
 using namespace virmar;
 
 void doit() {
-    
+    ll a = read<ll>(), b = read<ll>();
+    cout << a + b << endl;
 }
 
 int main() {
-    READ_ARC = false;
+    virmar::READ_ARG = true;
     // optimize();
-    int T = 1; cin >> T;
+    int T = 1;// cin >> T;
     while (T--) doit();
 }
 
--- P_1972_SDOI_2009_HH的项链.cpp
+++ P_1972_SDOI_2009_HH的项链.cpp
@@ -13,7 +13,7 @@ namespace virmar {
     using namespace std;
     using ll = long long;
 
-    const int N_ = 100000;
+    const int N_ = 1000000;
 
     bool READ_ARC = false;
 
@@ -66,14 +66,56 @@ namespace virmar {
 
 using namespace virmar;
 
+const int N = 1E6 + 6;
+
+int n, m, pos;
+int a[N], ans[N];
+int v[N];
+BinaryIndexedTree BIT;
+
+struct query{
+    int l, r, id;
+    bool operator<(struct query t) {
+        return r < t.r;
+    }
+} Q[N];
+
 void doit() {
-    
+    mset(v, 0);
+    n = read<int>();
+    for (int i = 1; i <= n; i++) a[i] = read<int>();
+    m = read<int>();
+    for (int i = 1; i <= m; i++) Q[i] = {read<int>(), read<int>(), i};
+
+    sort(Q + 1, Q + m + 1);
+
+    BIT.init(n);
+
+    pos = 1;
+    for (int i = 1; i <= m; i++) {
+        while (pos <= Q[i].r) {
+            if (v[a[pos]]) BIT.add(v[a[pos]], -1);
+            v[a[pos]] = pos;
+            BIT.add(pos, 1);
+            pos++;
+        }
+        ans[Q[i].id] = BIT.get(Q[i].r) - BIT.get(Q[i].l - 1);
+    }
+
+    for (int i = 1; i <= m; i++)
+        cout << ans[i] << endl;
+
+    // cout << n << endl;
+    // for (int i = 1; i <= n; i++) cout << a[i] << ' ';
+    // cout << endl << m << endl;
+    // for (int i = 1; i <= m; i++)
+    //     cout << Q[i].l << ' ' << Q[i].r << ' ' << Q[i].id << endl;
 }
 
 int main() {
-    READ_ARC = false;
+    READ_ARC = true;
     // optimize();
-    int T = 1; cin >> T;
+    int T = 1; // cin >> T;
     while (T--) doit();
 }
 
