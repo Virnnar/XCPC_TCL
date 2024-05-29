6e17fde8c1d80ac5cc2beb3287c932b0fc86c172
Virmar <virmarqy@gmail.com>
2024-05-30 05:12:00 +0800

    5.30

--- C_Nikita_and_LCM.cpp
+++ C_Nikita_and_LCM.cpp
@@ -7,6 +7,7 @@
 #include<unordered_map>
 #include<algorithm>
 #include<numeric>
+#include<iomanip>
 
 #define mset(a, t) memset(a, t, sizeof a)
 
@@ -38,6 +39,16 @@ namespace virmar {
         return x * f;
     }
 
+    template<typename T>
+    inline T gcd(T a, T b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    template<typename T>
+    inline T lcm(T a, T b) {
+        return a * b / gcd<T>(a, b);
+    }
+
     class BinaryIndexedTree{
     public:
         int n;
@@ -67,8 +78,46 @@ namespace virmar {
 
 using namespace virmar;
 
+int jud(vector<pair<int, int> > &a, int d) {
+    ll LCM = 0, res = 0;
+    for (auto [u, v]: a)
+        if (d % u == 0) {
+            if (LCM == 0) LCM = u;
+            else LCM = lcm<ll>(LCM, u);
+            res += v;
+        }
+    if (LCM != d) res = 0;
+    return res;
+}
+
 void doit() {
-    
+    int n = read<int>();
+    vector<int> a(n);
+    for (auto &t: a) cin >> t;
+    map<int, int> unique_element;
+    for (auto t: a) unique_element[t]++;
+    vector<pair<int, int> > b;
+    for (auto t: unique_element)
+        b.push_back(t);
+
+    int max_val = *max_element(a.begin(), a.end());
+    ll LCM = 1;
+    for (auto [u, v]: b) {
+        LCM = lcm<ll>(LCM, u);
+        if (LCM > max_val) return cout << n << endl, void();
+    }
+
+    int ans = 0;
+    for (int i = 1; i * i <= LCM; i++) {
+        if (LCM % i == 0) {
+            if (!unique_element.count(i))
+                ans = max(ans, jud(b, i));
+            if (!unique_element.count(LCM / i))
+                ans = max(ans, jud(b, LCM / i));
+        }
+    }
+    cout << ans << endl;
+
 }
 
 int main() {
